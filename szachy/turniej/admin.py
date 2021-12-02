b0234ba17414fd976a41edab2203586246921d8e
Mariusz <mario99.9@o2.pl>
2021-12-02 19:29:01 +0100

    models, db_schema improvements

--- szachy/turniej/admin.py
+++ szachy/turniej/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Sedzia, Uczestnik, Turniej, Rozgrywka
 
 # Register your models here.
+
+admin.site.register(Sedzia)
+admin.site.register(Uczestnik)
+admin.site.register(Turniej)
+admin.site.register(Rozgrywka)
--- szachy/turniej/models.py
+++ szachy/turniej/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+
+class Uczestnik(models.Model):
+    imie = models.CharField(max_length=45)
+    nazwisko = models.CharField(max_length=45)
+    nr_tel = models.IntegerField()
+    nr_email = models.CharField(max_length=45)
+
+    def __str__(self):
+        return self.imie + ' ' + self.nazwisko
+
+
+class Sedzia(models.Model):
+    imie = models.CharField(max_length=45)
+    nazwisko = models.CharField(max_length=45)
+
+    def __str__(self):
+        return self.imie + ' ' + self.nazwisko
+
+
+class Turniej(models.Model):
+    nazwa = models.CharField(max_length=45)
+    adres = models.CharField(max_length=45)
+
+    def __str__(self):
+        return self.nazwa
+
+
+class Rozgrywka(models.Model):
+    turniej = models.ForeignKey(Turniej, on_delete=models.CASCADE)
+    ucz1 = models.ForeignKey(Uczestnik, related_name='id1', on_delete=models.CASCADE)
+    ucz2 = models.ForeignKey(Uczestnik, related_name='id2', on_delete=models.CASCADE)
+    sedzia = models.ForeignKey(Sedzia, on_delete=models.CASCADE)
+    wynik_ucz1 = models.IntegerField()
+    wynik_ucz2 = models.IntegerField()
+
+    def __str__(self):
+        return self.ucz1.imie + ' ' + self.ucz1.nazwisko + ' vs ' + self.ucz2.imie + ' ' + self.ucz1.nazwisko
